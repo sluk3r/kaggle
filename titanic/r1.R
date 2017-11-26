b04e7895dc06f24f2442bd41dd1d44f4b075da26
wangxichun <wangxichun@jd.com>
2017-11-26 22:10:43 +0800

    titanic

---
+++ titanic/r1.R
@@ -0,0 +1,18 @@
+
+
+setwd("E:\openSrc\kaggle\titanic")
+
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
+
+str(train)
+# Look at number of people who survived
+table(train$Survived)
+prop.table(table(train$Survived))
+
+# Create new column in test set with our prediction that everyone dies
+test$Survived <- rep(0, 418)
+
+# Create submission dataframe and output to file
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
